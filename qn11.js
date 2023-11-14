82ca20f1199ef3e3791f141f90e0d6853e41fdde
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-11-14 14:29:17 +0200

    eleventh commit

---
+++ qn11.js
@@ -0,0 +1,14 @@
+/*Create a function that displays a pattern like this:
+1 1 1 1 1
+1 1 1
+1 1 
+1
+*/
+
+function trianglePatern(rows) {
+    for (let i = rows; i>=1 ; i--) {
+     console.log('1 '.repeat(i));
+   }
+   }
+   trianglePatern(4,5);
+   
